526491aae895cc5713d3400c4a429abea8109522
Ishi Ruy <dev@nhz.io>
2016-12-28 00:57:40 +0200

    added an easier observer

---
+++ src/102-an-easier-observer.js
@@ -0,0 +1,14 @@
+import {Observable} from 'rxjs'
+import console from './console'
+
+const numbers = [1, 5, 10]
+const source = Observable.from(numbers)
+
+console.info('An Easier Observer')
+source.subscribe(
+    val => console.log(`value: ${val}`),
+    err => console.log(`error: ${err.message || err}`),
+    () => console.info(`complete`)
+)
+console.log()
+
--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import console from './console'
 import './101-first-observable.js'
+import './102-an-easier-observer.js'
