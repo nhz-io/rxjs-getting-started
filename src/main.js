e02172c29ab76ff50042556dedfa5f304238e677
Ishi Ruy <dev@nhz.io>
2016-12-27 23:48:59 +0200

    console logger capability demo

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import console from './console'
+
+console.append('← Message count (Index resets at 999)')
+console.log('log')
+console.debug('debug')
+console.info('info')
+console.warn('warn')
+console.error('error')
