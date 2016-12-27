3ea0fa1b38c281c8552cc3eaf25a1285768d2eeb
Ishi Ruy <dev@nhz.io>
2016-12-28 00:36:34 +0200

    do not enumerate empty log

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import console from './console'
 
-console.append('← Message count (Index resets at 999)')
+console.append('←', ' Message count (Index resets at 999)')
+console.log()
 console.log('log')
 console.debug('debug')
 console.info('info')
