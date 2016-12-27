d343afaf67a153eb1340c454dcdc99eb9cfb4763
Ishi Ruy <dev@nhz.io>
2016-12-28 01:14:10 +0200

    added going async with setTimeout

---
+++ src/104-going-async-with-set-timeout.js
@@ -0,0 +1,28 @@
+import {Observable} from 'rxjs'
+import console from './console'
+
+const numbers = [1, 5, 10]
+const source = Observable.create(observer => {
+    let index = 0
+
+    const produceValue = () => {
+        observer.next(numbers[index++])
+
+        if (index < numbers.length) {
+            setTimeout(produceValue, 1000)
+        } else {
+            observer.complete()
+            console.log()
+        }
+    }
+
+    produceValue()
+})
+
+console.info('Going Async with setTimeout')
+source.subscribe(
+    val => console.log(`value: ${val}`),
+    err => console.error(`error: ${err.message || err}`),
+    () => console.info(`complete`)
+)
+
