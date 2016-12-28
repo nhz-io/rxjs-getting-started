333861ed28cd181b2084044381850fdaec00b066
Ishi Ruy <dev@nhz.io>
2016-12-28 03:03:01 +0200

    added run-gen

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import run from 'run-gen'
 import console from './console'
 
-console.append('←', ' Message count (Index resets at 999)')
-console.log()
-console.log('log')
-console.debug('debug')
-console.info('info')
-console.warn('warn')
-console.error('error')
+run(function* () {
+    console.append('←', ' Message count (Index resets at 999)')
+    console.log()
+    console.log('log')
+    console.debug('debug')
+    console.info('info')
+    console.warn('warn')
+    console.error('error')
+})
