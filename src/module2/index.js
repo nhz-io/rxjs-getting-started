d8f9fc485f3c64cc4c3637098648a4dce1121481
Ishi Ruy <dev@nhz.io>
2016-12-29 22:40:07 +0200

    added using flatmap to process innner observables

--- src/module2/index.js
+++ src/module2/index.js
@@ -1,4 +1,5 @@
 import p1 from './101-processing-mouse-events.js'
 import p2 from './102-sending-requests-with-xmlhttprequest.js'
+import p3 from './103-using-flatmap-to-process-inner-observables.js'
 
-export default [p1, p2]
+export default [p1, p2, p3]
