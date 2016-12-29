b2314842c67957744c55ee1bd4ce7c558019ae80
Ishi Ruy <dev@nhz.io>
2016-12-30 00:25:32 +0200

    added using fetch and promises

--- src/module2/index.js
+++ src/module2/index.js
@@ -2,5 +2,6 @@ import p1 from './101-processing-mouse-events.js'
 import p2 from './102-sending-requests-with-xmlhttprequest.js'
 import p3 from './103-using-flatmap-to-process-inner-observables.js'
 import p4 from './104-implementing-retry-logic-with-retry-and-retrywhen.js'
+import p5 from './105-using-fetch-and-promises.js'
 
-export default [p1, p2, p3, p4]
+export default [p1, p2, p3, p4, p5]
