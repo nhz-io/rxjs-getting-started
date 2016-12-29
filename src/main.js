df119a9a9351e9c604a0c1b9e92452a32c345150
Ishi Ruy <dev@nhz.io>
2016-12-29 20:57:15 +0200

    added processing mouse events

--- src/main.js
+++ src/main.js
@@ -2,21 +2,12 @@ import run from 'run-gen'
 
 import console from './console'
 
-import example from './example'
+import m2 from './module2'
 
 run(function* () {
-    console.append('←', ' Message count (Index resets at 999)')
-    console.log()
-    console.log('log')
-    console.debug('debug')
-    console.info('info')
-    console.warn('warn')
-    console.error('error')
-    console.log()
+    console.info('Module 2\n')
 
-    console.info('Running async example')
-    yield* example(console)
-    console.log()
-
-    console.info('Finished')
+    for (const p of m2) {
+        yield* p(console)
+    }
 }).catch(err => console.error(err.stack || err.message || err))
---
+++ src/module2/index.js
@@ -0,0 +1,3 @@
+import p1 from './101-processing-mouse-events.js'
+
+export default [p1]
