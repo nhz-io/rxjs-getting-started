8a74ee9abc38b5d6fb4221ff68c6efa2281dae10
Ishi Ruy <dev@nhz.io>
2016-12-29 23:42:55 +0200

    added implementing retry logic with retry and retrywhen

--- src/module2/index.js
+++ src/module2/index.js
@@ -1,5 +1,6 @@
 import p1 from './101-processing-mouse-events.js'
 import p2 from './102-sending-requests-with-xmlhttprequest.js'
 import p3 from './103-using-flatmap-to-process-inner-observables.js'
+import p4 from './104-implementing-retry-logic-with-retry-and-retrywhen.js'
 
-export default [p1, p2, p3]
+export default [p1, p2, p3, p4]
