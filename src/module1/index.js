ec3bfaed24898171f29dff9de46772dcc935191d
Ishi Ruy <dev@nhz.io>
2016-12-30 02:35:21 +0200

    dropped useless part

--- src/module1/index.js
+++ src/module1/index.js
@@ -3,6 +3,5 @@ import p2 from './102-an-easier-observer.js'
 import p3 from './103-using-observable-create.js'
 import p4 from './104-going-async-with-set-timeout.js'
 import p5 from './105-using-rxjs-operators.js'
-import p6 from './106-importing-just-what-we-need.js'
 
-export default [p1, p2, p3, p4, p5, p6]
+export default [p1, p2, p3, p4, p5]
